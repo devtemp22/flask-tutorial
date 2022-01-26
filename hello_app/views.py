767940c315f2909281d2bb7354ae17146683b948
Pruebas <cuentaempx22@gmail.com>
2022-01-26 13:07:34 -0600

    feat: what we do page added

--- hello_app/views.py
+++ hello_app/views.py
@@ -10,6 +10,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/wedo/")
+def wedo():
+    return render_template("wedo.html")
+
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
